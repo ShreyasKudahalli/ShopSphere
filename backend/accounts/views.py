fa6ecd2f5a0bdb9700903a47eaf19320b2ed8d23
ShreyasKudahalli <shreyaskudahalli1857@gmail.com>
2026-07-04 00:02:55 +0530

    feat: add authenticated user profile retrieval and update APIs

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -18,4 +18,11 @@ class MeView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request):
         serializer = CustomUserDetailSerializer(request.user)
-        return Response(serializer.data)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def patch(self, request):
+        serializer = CustomUserDetailSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
