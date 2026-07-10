b04445186732c98e1db74bda58765f87c6e10168
ShreyasKudahalli <shreyaskudahalli1857@gmail.com>
2026-07-10 19:15:07 +0530

    feat: add product detail API endpoint

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -11,4 +12,11 @@ class ProductListView(APIView):
     def get(self,request):
         products = Product.objects.all()
         serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+class ProductDetailView(APIView):
+
+    def get(self, request, id):
+        product = get_object_or_404(Product, id=id)
+        serializer = ProductSerializer(product)
         return Response(serializer.data,status=status.HTTP_200_OK)
