372e7a71a04c47f4bb98dd744e9ea0f0cc69e641
ShreyasKudahalli <shreyaskudahalli1857@gmail.com>
2026-07-13 21:47:38 +0530

    feat: add product creation API with admin-only access

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,15 +4,28 @@ from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 
 # Create your views here.
 class ProductListView(APIView):
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsAdminUser()]
+        return [AllowAny()]
     
     def get(self,request):
         products = Product.objects.all()
         serializer = ProductSerializer(products,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def post(self,request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class ProductDetailView(APIView):
 
